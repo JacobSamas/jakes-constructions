51b59c99d74a9397283fa5a0d571966755046ed4
Jacob Samas <samasjacob@icloud.com>
2025-01-02 14:43:14 +0530

    feat: Added colour pallet

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -8,8 +8,17 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: "#1E40AF",
+        secondary: "#9333EA",
+        accent: {
+          yellow: "#FACC15",
+          green: "#10B981",
+          red: "#EF4444",
+        },
+        background: {
+          light: "#F3F4F6",
+          dark: "#111827",
+        },
       },
     },
   },
